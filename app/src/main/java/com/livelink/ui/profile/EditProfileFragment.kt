9a0d1a3f37b3d3737c24ae9db0416ac08f4839d0
domischaen <99651391+domischaen@users.noreply.github.com>
2024-06-25 09:55:27 +0200

    Aktueller Stand.

--- app/src/main/java/com/livelink/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/com/livelink/ui/profile/EditProfileFragment.kt
@@ -1,9 +1,11 @@
 package com.livelink.ui.profile
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -17,6 +19,15 @@ class EditProfileFragment : Fragment() {
     private lateinit var binding: FragmentEditprofileBinding
     private val viewModel: SharedViewModel by activityViewModels()
 
+    private val getContent =
+        registerForActivityResult(
+            ActivityResultContracts.GetContent()
+        ) { uri: Uri? ->
+            uri?.let {
+                viewModel.uploadProfilePicture(it)
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,7 +40,7 @@ class EditProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.userData.observe(viewLifecycleOwner, Observer { user ->
+        viewModel.userData.observe(viewLifecycleOwner) { user ->
             binding.ProfileImageView.load(user.profilePicURL) {
                 placeholder(R.drawable.placeholder_profilepic)
                 error(R.drawable.placeholder_profilepic)
@@ -41,7 +52,11 @@ class EditProfileFragment : Fragment() {
             binding.EditGenderEditText.setText(user.gender)
             binding.EditRelationshipStatusEditText.setText(user.relationshipStatus)
 
+            binding.ProfileImageView.setOnClickListener {
+                getContent.launch("image/*")
+            }
 
-        })
+
+        }
     }
 }
