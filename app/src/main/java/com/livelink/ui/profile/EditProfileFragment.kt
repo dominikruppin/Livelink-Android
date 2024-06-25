34d68a48fdad85d0d717367c60e77bbd22f2ade7
Dominik Ruppin <domischaen@gmail.com>
2024-06-25 14:52:51 +0200

    Profil editieren erweitert um Spinner und Datepicker. Kleinere Anpassungen an Logo etc. - ToDo: Profil editieren fertigstellen, mit Speicherfunktion (+ Button)

--- app/src/main/java/com/livelink/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/com/livelink/ui/profile/EditProfileFragment.kt
@@ -8,11 +8,15 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
+import android.app.DatePickerDialog
+import android.util.Log
+import android.widget.DatePicker
+import java.util.*
 import coil.load
 import com.livelink.R
 import com.livelink.SharedViewModel
 import com.livelink.databinding.FragmentEditprofileBinding
+import java.text.SimpleDateFormat
 
 class EditProfileFragment : Fragment() {
 
@@ -49,14 +53,82 @@ class EditProfileFragment : Fragment() {
             binding.EditNameEditText.setText(user.name)
             binding.EditAgeEditText.setText(user.age.toString())
             binding.EditBirthdayEditText.setText(user.birthday)
-            binding.EditGenderEditText.setText(user.gender)
-            binding.EditRelationshipStatusEditText.setText(user.relationshipStatus)
+
+            val genderOptions = resources.getStringArray(R.array.gender_options)
+            if (user.gender.isNotEmpty()) {
+                val defaultIndex = genderOptions.indexOf(user.gender)
+                binding.EditGenderSpinner.setSelection(defaultIndex)
+                // binding.EditGenderSpinner.isEnabled = false
+            } else {
+                binding.EditGenderSpinner.isEnabled = true
+            }
 
             binding.ProfileImageView.setOnClickListener {
                 getContent.launch("image/*")
             }
 
+            binding.EditBirthdayEditText.setOnClickListener {
+                Log.d("Profil", "Birthday clicked")
+                showDatePickerDialog()
+            }
+
+
+
 
         }
     }
+
+    private fun showDatePickerDialog() {
+        Log.d("Profil", "DatePicker aufgerufen")
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val existingDate = binding.EditBirthdayEditText.text?.toString()
+        val dateParts = existingDate?.split(".")
+        val existingYear = dateParts?.get(2)?.toIntOrNull()
+        val existingMonth = (dateParts?.get(1)?.toIntOrNull() ?: month) - 1
+        val existingDay = dateParts?.get(0)?.toIntOrNull()
+
+        val datePickerDialog = DatePickerDialog(requireContext(), { _, year, monthOfYear, dayOfMonth ->
+            val selectedCalendar = Calendar.getInstance()
+            selectedCalendar.set(Calendar.YEAR, year)
+            selectedCalendar.set(Calendar.MONTH, monthOfYear)
+            selectedCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY)
+            val selectedDate = dateFormat.format(selectedCalendar.time)
+
+            binding.EditBirthdayEditText.setText(selectedDate)
+        }, existingYear ?: year, existingMonth, existingDay ?: day)
+
+        datePickerDialog.datePicker.firstDayOfWeek = Calendar.MONDAY
+
+        datePickerDialog.setButton(DatePickerDialog.BUTTON_POSITIVE, "OK") { dialog, which ->
+            val selectedYear = datePickerDialog.datePicker.year
+            val selectedMonth = datePickerDialog.datePicker.month
+            val selectedDayOfMonth = datePickerDialog.datePicker.dayOfMonth
+
+            val selectedCalendar = Calendar.getInstance()
+            selectedCalendar.set(Calendar.YEAR, selectedYear)
+            selectedCalendar.set(Calendar.MONTH, selectedMonth)
+            selectedCalendar.set(Calendar.DAY_OF_MONTH, selectedDayOfMonth)
+
+            val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY)
+            val selectedDate = dateFormat.format(selectedCalendar.time)
+
+            binding.EditBirthdayEditText.setText(selectedDate)
+        }
+
+        datePickerDialog.setButton(DatePickerDialog.BUTTON_NEGATIVE, "Abbrechen") { dialog, which ->
+            datePickerDialog.dismiss()
+        }
+        datePickerDialog.show()
+    }
+
+
+
+
+
+
 }
